99f4dd939c831794f450bf49604f7f25a6b01280
Mario Hoyos <mariohoyos92@gmail.com>
2018-04-26 14:11:33 -0500

    chore: completed section

---
+++ FollowingAlongWithUdemyCourse/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FollowingAlongWithUdemyCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person("Mario", "Hoyos", DateTime.Now, "Colombia" );
+            Console.WriteLine(personOne.FirstName);
+        }
+
+    }
+
+    class Person 
+    {
+        string firstName;
+        string lastName;
+        DateTime birthDate;
+        string country;
+
+        public Person(string fName,string lName,DateTime bDay,string country)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.birthDate = bDay;
+            this.country = country;
+
+        }
+
+
+
+
+        public string FirstName
+        {
+            get { return firstName;  }
+            set { firstName = value; }
+        }
+
+        public string LastName 
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public string Country
+        {
+            get { return Country; }
+            set { country = value; }
+        }
+    }
+}
